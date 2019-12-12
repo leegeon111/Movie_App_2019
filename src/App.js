8978db1bf0820a5293078943616eaa22fb56f110
leegeon111 <leegeon111@gitgub.com>
2019-12-12 11:10:52 +0900

    ex_componet

--- src/App.js
+++ src/App.js
@@ -1,43 +1,70 @@
 import React from 'react';
+import PropTypes from "prop-types";
 // import Potato from "./Potato";
 
-function Food ({name,picture})
+
+
+const foodLike  = [
+  {
+    id :1,
+    name : "kimchi",
+    image : "http://m.hansanggung.com/web/product/big/hansanggung1_44.jpg",
+    rating : 5.0
+  },
+  {
+    id :2,
+    name : "ramen",
+    image : "http://imgnews.naver.net/image/382/2014/12/21/68697033.2_99_20141221132203.jpg",
+    rating : 4.9
+  },
+  {
+    id :3,
+    name : "samgiopsal",
+    image : "https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F222d88e5c7dc496c8e8a8a56c3452e52.JPG",
+    rating : 4.4
+  }
+  ,
+  {
+    id :4,
+    name : "chukumi",
+    image : "https://craftlog.com/m/i/5979130=s1280=h960.webp",
+    rating : 4.7
+  }
+  ,
+  {
+    id :5,
+    name : "ckicken",
+    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20161021_163%2Fkjwkkht1125_1477007862250IDRkB_PNG%2Fclzlsajrwkclaor_%25281%2529.png&type=b400",
+    rating : 4.4
+  }
+]
+
+//반드시 전역변수로 받아서 넘겨줘야한다.
+function Food ({name, picture, rating})
 {
   //console.log({fav})
   //console.log(prob.fav);
   return (
     <div>
     <h1>I Like {name}</h1>
-    <img src = {picture}/>
+    <h4>{rating}/5.0</h4>
+    <img src = {picture} alt = {name}/>
     </div>
   )
 }
+//porptypes는 type 형태를 체크할 수있다.
+Food.propTypes ={
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+};
 
-const foodLike  = [
-  {
-    name : "kimchi",
-    image : "http://blogfiles.naver.net/MjAxNjEyMTZfMjk1/MDAxNDgxODU1NDM3MzE2.knBhLySz1SkewPg14I0f4T7fdnByzk8fudGFSdmcwY8g.pM2XrP9GZJ80OzBbF0xgYXdIA7Xdtyn8HR8_F-QgcXYg.JPEG.hwatongkc/%B8%C0%B1%E8%C4%A1640x640.jpg"
-  },
-  {
-    name : "ramen",
-    image : "http://imgnews.naver.net/image/382/2014/12/21/68697033.2_99_20141221132203.jpg"
-  },
-  {
-    name : "samgiopsal",
-    image : "http://blogfiles.naver.net/20160621_206/zmfsha77_1466499747478xfxc5_JPEG/%BC%DB%B5%B5%BB%F0%B0%E3%BB%EC%B8%C0%C1%FD_%2835%29.JPG"
-  }
-  ,
-  {
-    name : "chukumi",
-    image : "http://blogfiles.naver.net/20140113_170/qsd4898_1389573848504f8W1X_JPEG/%C1%F8%C1%D6%B8%C0%C1%FD_%C8%F1%BE%DF%B3%D7_%C2%DE%B2%D9%B9%CC9.jpg"
-  }
-  ,
-  {
-    name : "ckicken",
-    image : "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20161021_163%2Fkjwkkht1125_1477007862250IDRkB_PNG%2Fclzlsajrwkclaor_%25281%2529.png&type=b400"
-  }
-]
 
+// function renderFood (dish)
+// {
+//   //console.log(dish);
+//   return <Food name = {dish.name} picture = {dish.image}/>
+// }
 function App() {
   return (
     <div className="App">
@@ -46,11 +73,20 @@ function App() {
      <Food fav = "ramen" prob = {true} dfjdjfdj = {[1,2,3,4,"hello"]}/>
      <Food fav = "samgiopsal" prob = {true} dfjdjfdj = {[1,2,3,4,"hello"]}/>
      <Food fav = "chukumi" prob = {true} dfjdjfdj = {[1,2,3,4,"hello"]}/> */}
+    {/* {foodLike.map(renderFood)} */}
+
      {
-     foodLike.map(dish => 
-     <Food name = {dish.name} picture = {dish.image}/>)
+      //foodlike의 값들을 dish로 받아서 뿌려준다.
+     foodLike.map(dish =>
+     <Food 
+        key = {dish.id} 
+        name = {dish.name} 
+        picture = {dish.image}
+        rating = {dish.rating}
+     />)
      }
     </div>
+
   );
 }
 
